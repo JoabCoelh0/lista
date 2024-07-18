8e3ea370d4635ab3669342559288eb41fbbc125f
JoabCoelh0 <josejoab003@gmail.com>
2024-07-18 17:26:05 -0300

    att

--- v3/script/script.js
+++ v3/script/script.js
@@ -1,31 +1,32 @@
-
-function addlista(){
-    const carrinho = []
-    const armazem = document.getElementById('armazem')
-
-    
-    const nome = document.getElementById("nome")
-
-
-    let produto = {
-        nome: nome.value
-    }
-
-    carrinho.push(produto)
-    armazem.innerHTML += `<strong>${nome.value}</strong>, e vale um total de  <input class='valor_do _produto'> <br>`
-    
-    console.log(carrinho)
-
-}
-
 function add(){
+
     entradaPesquisa = document.querySelector("#search .input input");
     nomeProduto = document.querySelector("#resultados .informacoes .nome_produto h2");
+
+    let valor = document.getElementById('preco')
+
     resultado = document.querySelector("#resultados");
 
+    valor.innerHTML = `<input type="namber" id="valor_input">`
     nomeProduto.innerHTML = `${entradaPesquisa.value}`
     resultado.style.display = "flex"
 
 
-
 }
+
+
+let contador = 0;
+
+    function aumentar() {
+       contador++;
+        atualizaçãoValor();
+    }
+
+    function diminuir() {
+        contador--;
+        atualizaçãoValor();
+    }
+
+    function atualizaçãoValor() {
+        document.getElementById('quantidade').textContent = contador;
+    }
